d5f0162b871ed5b09e32e9bf7a27a7e632001b0a
Alian Hakim <alianhakim9@gmail.com>
2023-09-14 06:27:50 +0700

    change docker compose, add error handling

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,13 +1,14 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import noteRoutes from "./routes/note-routes";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/notes", noteRoutes);
 app.use((req: Request, res: Response, next: NextFunction) => {
-  next(Error("Endpoint not found :("));
+  next(createHttpError(404, "Endpoint not found :("));
 });
 
 // error handler
@@ -15,8 +16,12 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
   let errorMessage = "an unknown error occurred";
-  if (error instanceof Error) errorMessage = error.message;
-  res.status(500).json({
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({
     error: errorMessage,
   });
 });
--- back-end/src/routes/note-routes.ts
+++ back-end/src/routes/note-routes.ts
@@ -4,6 +4,7 @@ import * as NotesController from "../controllers/notes-controller";
 const router = express.Router();
 
 router.get("/", NotesController.getNotes);
+router.get("/:noteId", NotesController.getNote);
 router.post("/", NotesController.createNote);
 
 export default router;
